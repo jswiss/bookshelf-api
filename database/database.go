3e54d20423c7323d03df9709b73a72de0b7368a2
jswiss <swissjoshua@gmail.com>
2021-01-14 09:43:10 +0000

    rename local-env to localenv

--- database/database.go
+++ database/database.go
@@ -1,11 +1,32 @@
 package database
 
 import (
-	"gorm.io/gorm"
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jackc/pgx/v4"
 )
 
-// https://github.com/go-gorm/postgres
-db, err := gorm.Open(postgres.New(postgres.Config{
-  DSN: "user=gorm password=gorm dbname=gorm port=9920 sslmode=disable TimeZone=Asia/Shanghai",
-  PreferSimpleProtocol: true, // disables implicit prepared statement usage
-}), &gorm.Config{})
+func dbConnection() {
+	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+	dbName := localenv.readEnv
+
+	fmt.Printf("db name is %s\n", dbName)
+
+	defer conn.Close(context.Background())
+
+	var name string
+	var weight int64
+	err = conn.QueryRow(context.Background(), "select name, weight from widgets where id=$1", 42).Scan(&name, &weight)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println(name, weight)
+}
